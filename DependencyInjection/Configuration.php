7984e0d29333622d604936510b9434649bda8a9e
Björn May <bjoern.may.extern@lekkerland.com>
2020-10-08 16:02:14 +0200

    add config

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HansPeterOrding\NflfastrSymfonyBundle\DependecyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+	public function getConfigTreeBuilder()
+	{
+		$treeBuilder = new TreeBuilder('nflfastr_symfony');
+		$rootNode = $treeBuilder->getRootNode();
+
+		$rootNode
+			->children()
+				->arrayNode('sources')
+					->children()
+						->arrayNode('playByPlay')
+							->useAttributeAsKey(true)
+							->arrayPrototype()
+								->children()
+									->scalarNode('baseUrl')
+										->defaultValue('https://github.com/guga31bb/nflfastR-data')
+									->end()
+									->scalarNode('path')
+										->defaultValue('tree/master/data')
+									->end()
+								->end()
+							->end()
+						->end()
+						->arrayNode('roster')
+							->useAttributeAsKey(true)
+							->arrayPrototype()
+								->children()
+									->scalarNode('baseUrl')
+										->defaultValue('https://github.com/mrcaseb/nflfastR-roster')
+									->end()
+									->scalarNode('path')
+										->defaultValue('tree/master/data/seasons')
+									->end()
+								->end()
+							->end()
+						->end()
+					->end()
+				->end()
+			->end();
+
+		return $treeBuilder;
+	}
+}
---
+++ NflfastrSymfonyBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HansPeterOrding\NflfastrSymfonyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NflfastrSymfonyBundle extends Bundle
+{
+
+}
